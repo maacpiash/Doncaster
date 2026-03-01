8a0db965d001d364bde0d580b53450d6107776f4
Ahad Chowdhury <ahad@maacpiash.com>
2026-03-02 00:52:47 +1100

    fix: Specify port for backend, frontend, and Aspire

--- src/AppHost/AppHost.cs
+++ src/AppHost/AppHost.cs
@@ -2,7 +2,8 @@ using Aspire.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var backend = builder.AddProject<Projects.Todo_WebAPI>("backend");
+var backend = builder.AddProject<Projects.Todo_WebAPI>("backend")
+    .WithEndpoint("http", e => e.Port = 5273);
 
 var gateway = builder.AddProject<Projects.Todo_Gateway>("gateway")
     .WithReference(backend)
@@ -10,7 +11,7 @@ var gateway = builder.AddProject<Projects.Todo_Gateway>("gateway")
     .WithExternalHttpEndpoints();
 
 var frontend = builder.AddNpmApp("frontend", "../Frontend", "start")
-    .WithHttpEndpoint(env: "PORT")
+    .WithHttpEndpoint(port: 4200, env: "PORT")
     .WithExternalHttpEndpoints()
     .WithReference(backend)
     .WaitFor(backend);
