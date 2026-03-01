2fbbd32b7ab46f1b7ede00898bd2ceae041ac7f2
Ahad Chowdhury <ahad@maacpiash.com>
2026-03-02 00:54:27 +1100

    chore: Remove Gateway project from AppHost
    AppHost is only used in dev, while Gateway is only used in prod.

--- src/AppHost/AppHost.cs
+++ src/AppHost/AppHost.cs
@@ -5,11 +5,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 var backend = builder.AddProject<Projects.Todo_WebAPI>("backend")
     .WithEndpoint("http", e => e.Port = 5273);
 
-var gateway = builder.AddProject<Projects.Todo_Gateway>("gateway")
-    .WithReference(backend)
-    .WaitFor(backend)
-    .WithExternalHttpEndpoints();
-
 var frontend = builder.AddNpmApp("frontend", "../Frontend", "start")
     .WithHttpEndpoint(port: 4200, env: "PORT")
     .WithExternalHttpEndpoints()
