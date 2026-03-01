cd85188b8e5f4235830760d139eabe6e3ce8ed80
Ahad Chowdhury <ahad@maacpiash.com>
2026-03-02 01:04:01 +1100

    feat: Add prod setup
    This includes
    - Env setup
    - SSL cert setup
    - Docker and Docker Compose setup
    - Telemetry setup (Grafana, Prometheus, Loki, Tempo, OTel Collector)

--- src/Gateway/Program.cs
+++ src/Gateway/Program.cs
@@ -1,12 +1,21 @@
+using LettuceEncrypt;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
+
+if (!builder.Environment.IsDevelopment())
+    builder.Services.AddLettuceEncrypt()
+        .PersistDataToDirectory(new DirectoryInfo("/app/certs"), null);
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
     .AddServiceDiscoveryDestinationResolver();
 
 var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+    app.UseHttpsRedirection();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
